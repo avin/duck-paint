5435022a928bd90882118dacb9c8947bfefc3e3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-19 18:55:52 +0000

    Deploy avin/duck-paint to github.com/avin/duck-paint.git:gh-pages

---
+++ precache-manifest.84b78fa8b1536c0c7149e9f7143f7564.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023a5d438a3fd7ca1df11ae664fbb87b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbe8094a136a9bbd4b58",
+    "url": "./static/css/main.e97882ef.chunk.css"
+  },
+  {
+    "revision": "9d6fc22861109eea568c",
+    "url": "./static/js/2.45a1c903.chunk.js"
+  },
+  {
+    "revision": "6cb63ffe17e67e850925e07fe86fc8ec",
+    "url": "./static/js/2.45a1c903.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbe8094a136a9bbd4b58",
+    "url": "./static/js/main.320a4d12.chunk.js"
+  },
+  {
+    "revision": "b9be15f61ed076da3843",
+    "url": "./static/js/runtime-main.b3eb5dfd.js"
+  },
+  {
+    "revision": "210a8d8a898a94b41a8ee26754de1da8",
+    "url": "./static/media/cog.210a8d8a.svg"
+  },
+  {
+    "revision": "be9786c25a7cd8dc72ae603afe51b153",
+    "url": "./static/media/duck.be9786c2.svg"
+  },
+  {
+    "revision": "5eac3b27d7f103a3396c51132ef4510c",
+    "url": "./static/media/question.5eac3b27.svg"
+  }
+]);
